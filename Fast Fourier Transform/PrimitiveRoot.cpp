f9dcbb96aff4d3bcca14a7166e205b1015c2a558
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2020-05-11 04:52:22 +0600

    Create PrimitiveRoot.cpp

---
+++ Fast Fourier Transform/PrimitiveRoot.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#ifdef VAMP
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+const LL MOD = 786433;
+LL BMod(LL B,LL P,LL M)
+{
+    LL R=1;
+    B=B%M;
+    while(P)
+    {
+        if(P&1)
+            R=(R*B)%M;
+        P >>= 1;
+        B=(B*B)%M;
+    }
+    return R;
+}
+LL primitive_root(LL p){
+    vector<LL>factor;
+    LL phi = p-1;
+    LL n = phi;
+
+    for(LL i=2;i*i<=n;i++){
+        if(n%i==0){
+            factor.push_back(i);
+            while(n%i==0) n/=i;
+        }
+    }
+    if(n>1) factor.push_back(n);
+
+    for(LL res=2;res<=p;res++){
+        bool ok=true;
+        for(LL i=0;i<factor.size();i++){
+            ok &= (BMod(res,phi/factor[i],MOD)!=1);
+        }
+        if(ok) return res;
+    }
+    return -1;
+}
+int main()
+{
+    cout << primitive_root(MOD)<< endl;
+    return 0;
+}
