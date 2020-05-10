cd325b857ef621d31d5b9f42a73a2fba26937995
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2020-05-11 04:55:50 +0600

    Create NTT.cpp

---
+++ Fast Fourier Transform/NTT.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#ifdef VAMP
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+const int G = 3;
+const int MOD =  998244353;
+const int N = (1 << 18) + 5;
+
+int rev[N], w[N], inv_n;
+
+int bigMod (int a, int e, int mod) {
+  if (e == -1) e = mod - 2;
+  int ret = 1;
+  while (e) {
+    if (e & 1) ret = (LL) ret * a % mod;
+    a = (LL) a * a % mod; e >>= 1;
+  }
+  return ret;
+}
+
+void prepare (int &n) {
+  int sz = abs(31 - __builtin_clz(n));
+  int r = bigMod(G, (MOD - 1) / n, MOD);
+  inv_n = bigMod(n, MOD - 2, MOD), w[0] = w[n] = 1;
+  for (int i = 1; i < n; ++i) w[i] = (LL) w[i - 1] * r % MOD;
+  for (int i = 1; i < n; ++i) rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (sz - 1));
+}
+
+void ntt (int *a, int n, int dir) {
+  for (int i = 1; i < n - 1; ++i) {
+    if (i < rev[i]) swap(a[i], a[rev[i]]);
+  }
+  for (int m = 2; m <= n; m <<= 1) {
+    for (int i = 0; i < n; i += m) {
+      for (int j = 0; j < (m >> 1); ++j) {
+        int &u = a[i + j], &v = a[i + j + (m >> 1)];
+        int t = (LL) v * w[dir ? n - n / m * j : n / m * j] % MOD;
+        v = u - t < 0 ? u - t + MOD : u - t;
+        u = u + t >= MOD ? u + t - MOD : u + t;
+      }
+    }
+  } if (dir) for (int i = 0; i < n; ++i) a[i] = (LL) a[i] * inv_n % MOD;
+}
+
+int f_a[N], f_b[N];
+
+vector <int> multiply (vector <int> a, vector <int> b) {
+  int sz = 1, n = a.size(), m = b.size();
+  while (sz < n + m - 1) sz <<= 1; prepare(sz);
+  for (int i = 0; i < sz; ++i) f_a[i] = i < n ? a[i] : 0;
+  for (int i = 0; i < sz; ++i) f_b[i] = i < m ? b[i] : 0;
+  ntt(f_a, sz, 0); ntt(f_b, sz, 0);
+  for (int i = 0; i < sz; ++i) f_a[i] = (LL) f_a[i] * f_b[i] % MOD;
+  ntt(f_a, sz, 1); return vector <int> (f_a, f_a + n + m - 1);
+}
+
+
+int main()
+{
+    #ifdef VAMP
+        clock_t tStart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    vector<int>A,B;
+    A.push_back(1);
+    A.push_back(1);
+
+    B.push_back(2);
+    B.push_back(1);
+
+    vector<int>C = multiply(A,B);
+
+    for(int i : C)cout << i << " ";
+
+    #ifdef VAMP
+        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+}
