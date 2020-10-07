e0ee2f16ce55cfdfc4ba33dead28fc3b62f08d68
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2020-10-07 12:48:20 +0600

    Create Lagrange Verified.cpp

---
+++ Mathematics/Lagrange Verified.cpp
@@ -0,0 +1,147 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#ifdef VAMP
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+const LL mod = 10007;
+template <int32_t MOD>
+struct modint {
+    int32_t value;
+    modint() = default;
+    modint(int32_t value_) : value(value_) {}
+    inline modint<MOD> operator + (modint<MOD> other) const { int32_t c = this->value + other.value; return modint<MOD>(c >= MOD ? c - MOD : c); }
+    inline modint<MOD> operator - (modint<MOD> other) const { int32_t c = this->value - other.value; return modint<MOD>(c <    0 ? c + MOD : c); }
+    inline modint<MOD> operator * (modint<MOD> other) const { int32_t c = (int64_t)this->value * other.value % MOD; return modint<MOD>(c < 0 ? c + MOD : c); }
+    inline modint<MOD> & operator += (modint<MOD> other) { this->value += other.value; if (this->value >= MOD) this->value -= MOD; return *this; }
+    inline modint<MOD> & operator -= (modint<MOD> other) { this->value -= other.value; if (this->value <    0) this->value += MOD; return *this; }
+    inline modint<MOD> & operator *= (modint<MOD> other) { this->value = (int64_t)this->value * other.value % MOD; if (this->value < 0) this->value += MOD; return *this; }
+    inline modint<MOD> operator - () const { return modint<MOD>(this->value ? MOD - this->value : 0); }
+    modint<MOD> pow(uint64_t k) const {
+        modint<MOD> x = *this, y = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) y *= x;
+            x *= x;
+        }
+        return y;
+    }
+    modint<MOD> inv() const { return pow(MOD - 2); }  // MOD must be a prime
+    inline modint<MOD> operator /  (modint<MOD> other) const { return *this *  other.inv(); }
+    inline modint<MOD> operator /= (modint<MOD> other)       { return *this *= other.inv(); }
+    inline bool operator == (modint<MOD> other) const { return value == other.value; }
+    inline bool operator != (modint<MOD> other) const { return value != other.value; }
+    inline bool operator < (modint<MOD> other) const { return value < other.value; }
+    inline bool operator > (modint<MOD> other) const { return value > other.value; }
+};
+template <int32_t MOD> modint<MOD> operator * (int64_t value, modint<MOD> n) { return modint<MOD>(value) * n; }
+template <int32_t MOD> modint<MOD> operator * (int32_t value, modint<MOD> n) { return modint<MOD>(value % MOD) * n; }
+template <int32_t MOD> ostream & operator << (ostream & out, modint<MOD> n) { return out << n.value; }
+
+using mint = modint<mod>;
+
+mint Lagrange(const vector<mint> &p, mint x, mint a = 0, mint d = 1) {
+    int n = p.size() - 1;
+    if (a == 0 and d == 1 and x.value <= n) return p[x.value];
+
+    vector<mint> pref(n + 1, 1), suf(n + 1, 1);
+    for (int i = 0; i < n; i++) pref[i + 1] = pref[i] * (x - (a + d * i));
+    for (int i = n; i > 0; i--) suf[i - 1] = suf[i] * (x - (a + d * i));
+
+    vector<mint> fact(n + 1, 1),finv(n + 1, 1);
+    for (int i = 1; i <= n; i++) fact[i] = fact[i - 1] * d * i;
+
+    for (int i = n; i >= 0; i--) finv[i] = mint(fact[i]).pow(mod - 2);
+
+    mint ans = 0;
+    for (int i = 0; i <= n; i++) {
+        mint tmp = p[i] * pref[i] * suf[i] * finv[i] * finv[n-i];
+        if ((n - i) & 1) ans -= tmp;
+        else ans += tmp;
+    }
+
+    return ans;
+}
+
+int main()
+{
+    #ifdef VAMP
+        clock_t tStart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    LL n;
+    vector<mint> p;
+    for(n = 1;n <= 15;n++){
+        LL ans = 0;
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++){
+                for(int k = 1;k <= n;k++){
+                    for(int l = 1;l <= n;l++){
+                        for(int m = 1;m <= n;m++){
+                            LL temp = ABS(i - j) * ABS(j - k) * ABS(k - l) * ABS(l - m) * ABS(m - i);
+                            ans = (ans + temp) % mod;
+                        }
+                    }
+                }
+            }
+        }
+        p.push_back(ans);
+    }
+    while(cin >> n){
+        if(n == 0)break;
+        mint x = n - 1;
+        mint ans = Lagrange(p,x);
+        cout << ans << "\n";
+    }
+    #ifdef VAMP
+        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+}
