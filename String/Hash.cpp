65f47898db9072befca660e6d2e8d0dce7e96db0
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2020-12-31 07:27:02 +0600

    Create Hash.cpp

---
+++ String/Hash.cpp
@@ -0,0 +1,129 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fRead(x)        freopen(x,"r",stdin)
+#define fWrite(x)       freopen (x,"w",stdout)
+
+#define LL              long long
+#define ULL             unsigned long long
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define PI              acos(-1.0)
+#define mk              make_pair
+#define pii             pair<int,int>
+#define pll             pair<LL,LL>
+
+
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define SQR(a)          ((a)*(a))
+#define FOR(i,a,b)      for(int i=a;i<=b;i++)
+#define ROF(i,a,b)      for(int i=a;i>=b;i--)
+#define REP(i,b)        for(int i=0;i<b;i++)
+#define MEM(a,x)        memset(a,x,sizeof(a))
+#define ABS(x)          ((x)<0?-(x):(x))
+#define all(v) v.begin(),v.end()
+#define SORT(v)         sort(v.begin(),v.end())
+#define REV(v)          reverse(v.begin(),v.end())
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define FastRead        ios_base::sync_with_stdio(0);cin.tie(nullptr);
+#ifdef VAMP
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cout << name << " = " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cout.write(names, comma - names) << " = " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+
+
+const int N = 1700000;
+const int MOD = 1e9 + 7;
+const LL P[] = {97, 1000003};
+
+LL bigMod (LL a, LL e) {
+  if (e == -1) e = MOD - 2;
+  LL ret = 1;
+  while (e) {
+    if (e & 1) ret = ret * a % MOD;
+    a = a * a % MOD, e >>= 1;
+  }
+  return ret;
+}
+
+int pwr[2][N], inv[2][N];
+
+void initHash() {
+  for (int it = 0; it < 2; ++it) {
+    pwr[it][0] = inv[it][0] = 1;
+    LL INV_P = bigMod(P[it], -1);
+    for (int i = 1; i < N; ++i) {
+      pwr[it][i] = (LL) pwr[it][i - 1] * P[it] % MOD;
+      inv[it][i] = (LL) inv[it][i - 1] * INV_P % MOD;
+    }
+  }
+}
+
+//Call initHash. The functions are 0 indexed.
+struct RangeHash {
+  vector <int> h[2], rev[2];
+
+  RangeHash (const string S, bool revFlag = 0) {
+    for (int it = 0; it < 2; ++it) {
+      h[it].resize(S.size() + 1, 0);
+      for (int i = 0; i < S.size(); ++i) {
+        h[it][i + 1] = (h[it][i] + (LL) pwr[it][i + 1] * (S[i] - 'a' + 1)) % MOD;
+      }
+      if (revFlag) {
+        rev[it].resize(S.size() + 1, 0);
+        for (int i = 0; i < S.size(); ++i) {
+          rev[it][i + 1] = (rev[it][i] + (LL) inv[it][i + 1] * (S[i] - 'a' + 1)) % MOD;
+        }
+      }
+    }
+  }
+
+  inline LL get (int l, int r) {
+    if (l > r) return 0;
+    LL one = (LL) (h[0][r + 1] - h[0][l]) * inv[0][l + 1] % MOD;
+    LL two = (LL) (h[1][r + 1] - h[1][l]) * inv[1][l + 1] % MOD;
+    if (one < 0) one += MOD; if (two < 0) two += MOD;
+    return one << 31 | two;
+  }
+
+  inline LL getReverse (int l, int r) {
+    if (l > r) return 0;
+    LL one = (LL) (rev[0][r + 1] - rev[0][l]) * pwr[0][r + 1] % MOD;
+    LL two = (LL) (rev[1][r + 1] - rev[1][l]) * pwr[1][r + 1] % MOD;
+    if (one < 0) one += MOD; if (two < 0) two += MOD;
+    return one << 31 | two;
+  }
+};
+int main()
+{
+    #ifdef VAMP
+        clock_t tStart = clock();
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    #ifdef VAMP
+        fprintf(stderr, "\n>> Runtime: %.10fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
+    #endif
+}
