f3b454202da3f555c8c14e7b957dd79dad3aa466
Fahim Shahriar Shakkhor <fsshakkhor@gmail.com>
2022-04-15 15:26:05 +0600

    Create Modular Exponentiation.cpp

---
+++ Number Theory/Modular Exponentiation.cpp
@@ -0,0 +1,14 @@
+long long BigMod(long long B,long long P,long long M)
+{
+    long long R=1;
+    while(P>0)
+    {
+        if(P%2==1)
+        {
+            R=(R*B)%M;
+        }
+        P/=2;
+        B=(B*B)%M;
+    }
+    return R;
+}
